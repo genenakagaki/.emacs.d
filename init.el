d36aa34ea16eaef864947fa284d0bef581aee024
Gene Nakagaki <genenakagaki@Genes-MacBook-Air.local>
2023-05-28 10:18:31 +0900

    update config

--- init.el
+++ init.el
@@ -356,145 +356,26 @@ This functions should be added to the 'org-mode-hook'."
   (interactive)
   (find-file (concat org-roam-directory "/todo.org")))
 
-;;;###autoload
-(defun gn-org/dwim-at-point (&optional arg)
-  "Do-what-I-mean at point.
-If on a:
-- checkbox list item or todo heading: toggle it.
-- citation: follow it
-- headline: cycle ARCHIVE subtrees, toggle latex fragments and inline images in
-  subtree; update statistics cookies/checkboxes and ToCs.
-- clock: update its time.
-- footnote reference: jump to the footnote's definition
-- footnote definition: jump to the first reference of this footnote
-- timestamp: open an agenda view for the time-stamp date/range at point.
-- table-row or a TBLFM: recalculate the table's formulas
-- table-cell: clear it and go into insert mode. If this is a formula cell,
-  recaluclate it instead.
-- babel-call: execute the
-- statistics-cookie: update it.
-- src block: execute it
-- latex fragment: toggle it.
-- link: follow it
-- otherwise, refresh all inline images in current tree."
-  (interactive "P")
-  (if (button-at (point))
-      (call-interactively #'push-button)
-    (let* ((context (org-element-context))
-           (type (org-element-type context)))
-      ;; skip over unimportant contexts
-      (while (and context (memq type '(verbatim code bold italic underline strike-through subscript superscript)))
-        (setq context (org-element-property :parent context)
-              type (org-element-type context)))
-      (pcase type
-        ((or `citation `citation-reference)
-         (org-cite-follow context arg))
+(defun gn/org-dwim-at-point ()
+  (interactive)
+  (message "gn/org-dwim-at-point")
+  (let* ((element (org-element-at-point))
+         (context (org-element-context)))
+    (message "org-element-at-point")
+    (pp element)
+    (message "org-element-context")
+    (pp context)
+    (message "org-element-contents")
+    (pp (org-element-contents element))
+    (message "org-element-type")
+    (pp (org-element-type element))
 
-        (`headline
-         (cond ((memq (bound-and-true-p org-goto-map)
-                      (current-active-maps))
-                (org-goto-ret))
-               ((and (fboundp 'toc-org-insert-toc)
-                     (member "TOC" (org-get-tags)))
-                (toc-org-insert-toc)
-                (message "Updating table of contents"))
-               ((string= "ARCHIVE" (car-safe (org-get-tags)))
-                (org-force-cycle-archived))
-               ((or (org-element-property :todo-type context)
-                    (org-element-property :scheduled context))
-                (org-todo
-                 (if (eq (org-element-property :todo-type context) 'done)
-                     (or (car (+org-get-todo-keywords-for (org-element-property :todo-keyword context)))
-                         'todo)
-                   'done))))
-         ;; Update any metadata or inline previews in this subtree
-         (org-update-checkbox-count)
-         (org-update-parent-todo-statistics)
-         (when (and (fboundp 'toc-org-insert-toc)
-                    (member "TOC" (org-get-tags)))
-           (toc-org-insert-toc)
-           (message "Updating table of contents"))
-         (let* ((beg (if (org-before-first-heading-p)
-                         (line-beginning-position)
-                       (save-excursion (org-back-to-heading) (point))))
-                (end (if (org-before-first-heading-p)
-                         (line-end-position)
-                       (save-excursion (org-end-of-subtree) (point))))
-                (overlays (ignore-errors (overlays-in beg end)))
-                (latex-overlays
-                 (cl-find-if (lambda (o) (eq (overlay-get o 'org-overlay-type) 'org-latex-overlay))
-                             overlays))
-                (image-overlays
-                 (cl-find-if (lambda (o) (overlay-get o 'org-image-overlay))
-                             overlays)))
-           (+org--toggle-inline-images-in-subtree beg end)
-           (if (or image-overlays latex-overlays)
-               (org-clear-latex-preview beg end)
-             (org--latex-preview-region beg end))))
-
-        (`clock (org-clock-update-time-maybe))
-
-        (`footnote-reference
-         (org-footnote-goto-definition (org-element-property :label context)))
-
-        (`footnote-definition
-         (org-footnote-goto-previous-reference (org-element-property :label context)))
-
-        ((or `planning `timestamp)
-         (org-follow-timestamp-link))
-
-        ((or `table `table-row)
-         (if (org-at-TBLFM-p)
-             (org-table-calc-current-TBLFM)
-           (ignore-errors
-             (save-excursion
-               (goto-char (org-element-property :contents-begin context))
-               (org-call-with-arg 'org-table-recalculate (or arg t))))))
-
-        (`table-cell
-         (org-table-blank-field)
-         (org-table-recalculate arg)
-         (when (and (string-empty-p (string-trim (org-table-get-field)))
-                    (bound-and-true-p evil-local-mode))
-           (evil-change-state 'insert)))
-
-        (`babel-call
-         (org-babel-lob-execute-maybe))
-
-        (`statistics-cookie
-         (save-excursion (org-update-statistics-cookies arg)))
-
-        ((or `src-block `inline-src-block)
-         (org-babel-execute-src-block arg))
-
-        ((or `latex-fragment `latex-environment)
-         (org-latex-preview arg))
-
-        (`link
-         (let* ((lineage (org-element-lineage context '(link) t))
-                (path (org-element-property :path lineage)))
-           (if (or (equal (org-element-property :type lineage) "img")
-                   (and path (image-type-from-file-name path)))
-               (+org--toggle-inline-images-in-subtree
-                (org-element-property :begin lineage)
-                (org-element-property :end lineage))
-             (org-open-at-point arg))))
-
-        (`paragraph
-         (+org--toggle-inline-images-in-subtree))
-
-        ((guard (org-element-property :checkbox (org-element-lineage context '(item) t)))
-         (let ((match (and (org-at-item-checkbox-p) (match-string 1))))
-           (org-toggle-checkbox (if (equal match "[ ]") '(16)))))
-
-        (_
-         (if (or (org-in-regexp org-ts-regexp-both nil t)
-                 (org-in-regexp org-tsr-regexp-both nil  t)
-                 (org-in-regexp org-link-any-re nil t))
-             (call-interactively #'org-open-at-point)
-           (+org--toggle-inline-images-in-subtree
-            (org-element-property :begin context)
-            (org-element-property :end context))))))))
+    (pcase (-first-item element)
+      ('paragraph (let* ((parent (org-element-property :parent paragraph)))
+                    (message "at paragraph")
+                    ))
+    )
+  )
 
 (defun gn/org-fold-lines ()
   ;; This needs to be nil on order for 'toggle-truncate-lines' to work.
@@ -538,13 +419,22 @@ If on a:
   (evil-org-set-key-theme '(navigation insert textobjects additional calendar))
   (evil-org-agenda-set-keys))
 
+(cl-defmethod org-roam-node-gn-node-display ((node org-roam-node))
+  "Method used to display the org-roam node in the minibuffer."
+  (let ((title (org-roam-node-title node))
+        (file-title (org-roam-node-file-title node)))
+    (if (string= title file-title)
+        title
+      (concat file-title ": " title))))
+
 (use-package org-roam
   :after org
   :config
   (setq org-roam-directory "~/org-roam")
   (setq org-roam-dailies-directory "journal")
   (setq org-roam-db-location (concat org-roam-directory "/org-roam.db"))
-  (org-roam-db-autosync-mode))
+  (org-roam-db-autosync-mode)
+  (setq org-roam-node-display-template "${gn-node-display}"))
 
 (use-package websocket
   :after org-roam)
@@ -585,7 +475,7 @@ If on a:
 
 (general-def 'n org-mode-map
   ;; General org-mode usage
-  "RET" 'gn-org/dwim-at-point
+  "RET" 'org-ctrl-c-ctrl-c
   "M-h" 'org-metaleft
   "M-H" 'org-shiftmetaleft
   "M-l" 'org-metaright
@@ -599,7 +489,17 @@ If on a:
   "M-w" 'org-edit-src-abort
   "M-q" 'save-buffers-kill-terminal)
 
-(use-package plantuml-mode)
+(use-package plantuml-mode
+  :config
+  (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
+  )
+
+(use-package yaml-mode
+  :config
+  (add-to-list 'auto-mode-alist
+               '("\\.yml\\'" . yaml-mode)))
+
+(use-package docker)
 
 (general-def 'n 'override
   :prefix gn/leader-key
