db5c01de1cd33e5e5e97875f5a19e97bb1e46674
Gene Nakagaki <g-nakagaki@labol.co.jp>
2024-09-13 18:06:18 +0900

    add case converter

---
+++ scripts/case_converter.clj
@@ -0,0 +1,39 @@
+#!/usr/bin/env bb
+
+(defn kebab? [text]
+  (some? (re-find #"-" text)))
+
+(defn camel? [text]
+  (some? (re-find #"^[a-z].*[A-Z]" text)))
+
+(defn pascal? [text]
+  (some? (re-find #"^[A-Z].*[a-z]" text)))
+
+(let [[case-type
+       ;; "snake"
+       ;; "camel"
+       ;; "pascal"
+       text
+       ;; "this-is-a-test"
+       ;; "ThisIsATest"
+       ;; "thisIsATest"
+       ]
+      *command-line-args*
+
+      
+      ]
+  (let [words (cond
+                (kebab? text) (str/split text #"-")
+                (pascal? text) (->> (re-seq #"[A-Z][a-z]*" text)
+                                    (map str/lower-case))
+                (camel? text) (cons (re-find #"^[a-z]*" text)
+                                    (->> (re-seq #"[A-Z][a-z]*" text)
+                                         (map str/lower-case))))]
+    (-> (case case-type
+          "snake" (->> words
+                       (str/join "-"))
+          "camel" words
+          "pascal" words)
+        println))
+  )
+
